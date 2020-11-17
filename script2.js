00bfb80ef0324ff8a02cb5d5f5b51f809f5fc661
Jane <jane@jane>
2020-11-17 15:17:08 +0500

    dz

--- script2.js
+++ script2.js
@@ -3,10 +3,10 @@ var first = +prompt('Введите первое число!')
 var second = +prompt('Введите второе число!')  
 var third = +prompt('Введите третье число!') 
 
-if(first > second || first < third && second < third || second > third){
+if(first > second && first < third || second < third && second > third){
     alert(first + ' среднее число!')
-} else if(second > first || second < third && third > first || first > third){
+} else if(second > first && second < third || third > first && first > third){
     alert(second + ' среднее число!')
-} else if(third > second || third < first && first > second ||  first < second){
+} else if(third > second && third < first || first > second && first < second){
     alert(third + ' среднее число!')
 }
