ee5fc691a433d6bbfa36806ef961cd466c6a3e63
Jane <jane@jane>
2020-11-17 15:33:53 +0500

    final

--- script2.js
+++ script2.js
@@ -1,12 +1,13 @@
 
-var first = +prompt('Введите первое число!') 
-var second = +prompt('Введите второе число!')  
-var third = +prompt('Введите третье число!') 
+var first = +prompt('Введите первое число!');1
+var second = +prompt('Введите второе число!')  ;2
+var third = +prompt('Введите третье число!') ;5
 
-if(first > second && first < third || second < third && second > third){
+
+if(first > second && first < third || first < second && first > third){
     alert(first + ' среднее число!')
-} else if(second > first && second < third || third > first && first > third){
+} else if(second < first && second > third || second > first && second < third){
     alert(second + ' среднее число!')
-} else if(third > second && third < first || first > second && first < second){
+} else if(third < second && third > first || first > second && second < third){
     alert(third + ' среднее число!')
 }
